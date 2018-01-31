612522f2a7e237094899bec148bf8d45d9b159b4
Evyn N. Baldwin <evynparnell@live.co.uk>
2018-01-31 12:11:30 -0800

    Automation

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Watering Automation</title>
+  </head>
+  <body>
+    <table border="1">
+     <tr>
+        <th>Plant 0</th>
+        <th>Plant 1</th>
+        <th>Plant 2</th>
+        <th>Plant 3</th>
+        <th>Plant 4</th>
+        <th>Plant 5</th>
+        <th>Plant 6</th>
+        <th>Plant 7</th>
+        <th>Plant 8</th>
+        <th>Plant 9</th>
+        <th>Plant 10</th>
+        <th>Plant 11</th>
+        <th>Plant 12</th>
+        <th>Plant 13</th>
+        <th>Plant 14</th>
+        <th>Plant 15</th>
+     </tr>
+     <tr>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+       <td><img src="images/off.jpg" alt="off" /></td>
+     </tr>
+
+    <form action="Translation.php" method="post">
+      <tr>
+        <td><input type="submit" name="P1" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P2" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P3" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P4" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P5" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P6" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P7" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P8" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P9" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P10" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P11" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P12" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P13" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P14" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P15" value="Toggle" onclick="insert()" /></td>
+        <td><input type="submit" name="P16" value="Toggle" onclick="insert()" /></td>
+      </tr>
+    </form>
+  </table>
+  </body>
+</html>
